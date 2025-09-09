943a8b1b89de6bbe5a5f2c96ab75db4b0358f139
AmanKumar9122 <amanraj15112003@gmail.com>
2025-09-09 11:32:49 +0530

    created a new header file and imported it in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// core javascript stuff 
+import Header from "./Header"
+function App(){
+  return(
 
-function App() {
-  const [count, setCount] = useState(0)
+    // on removing div it's showing error.
+    // For using multiple h1 tags we need to wrap them inside a div element.
 
-  return (
-    <>
-      <h1>Aman Kumar</h1>
-    </>
+    <div>
+      <Header />
+      <h1>Hello React</h1>
+      <h1>Code by Aman Kumar</h1>
+
+    </div>
   )
 }
 
 export default App
+
+// work of react it to write html code in javascript file and render it over browser
+
+// if we write app in small letters and try to export it and use it in main.jsx it will not works.
+// As html tags are written in small letters. So react consider functions with capital letter as 
+// its component.
+
+
